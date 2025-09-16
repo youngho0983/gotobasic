6daf294c412fa7141d09cbed3683669ad70c9bcc
서영호의노가다\young <youngho0983@naver.com>
2025-09-16 17:19:14 +0900

    spring 쪽 설정

---
+++ goto_basic_spring/gotobasic/src/main/java/com/syh/gotobasic/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.syh.gotobasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+
+    @GetMapping("/")
+    public String test() {
+        return "Hello World!";
+    }
+
+}
---
+++ goto_basic_spring/gotobasic/src/test/java/com/syh/gotobasic/GotobasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syh.gotobasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotobasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
